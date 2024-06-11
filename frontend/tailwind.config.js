1be02930dae8ae59211c29436dd234424c2451bd
Arthur <155009708+ArthurGosi2024@users.noreply.github.com>
2024-06-11 13:54:36 -0300

    settings:added new colors

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,29 @@
 export default {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
-		extend: {},
+		extend: {
+
+			screens : {
+				'2xl-medium': '970px', 	
+			},
+			colors: {
+				'amber-300': "#FABA48 ",
+			},
+			fontFamily: {
+				inter: ["Inter", "Poppins"],
+			},
+
+			backgroundImage: {
+				"gradient-img":
+					" linear-gradient(0deg, #11131D 51%, rgba(17, 19, 29, 0.00) 70%)",
+			},
+			backgroundColor: {
+				cda: "#11131D",
+				'background-input': "rgba(27, 33, 49, 0.50)",
+				"background-home": "#E7E5E4",
+				"details-home": "#FABA48",
+			},
+		},
 	},
 	plugins: [],
 };

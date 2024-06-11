708480a3b024dab759b9232fc4558f5e29f51cd2
Arthur <155009708+ArthurGosi2024@users.noreply.github.com>
2024-06-11 01:47:13 -0300

    refactor: modifying the system for routing

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,27 @@
-import ReactDOM from "react-dom/client";
-
 import "./index.css";
-import App from "./App";
+import { createRoot } from "react-dom/client";
+import {
+	createBrowserRouter,
+	RouterProvider,
+	createRoutesFromElements,
+	Route,
+} from "react-router-dom";
+import Layout from "./pages/layout";
+import Login from "./pages/login/page";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
+const router = createBrowserRouter(
+	createRoutesFromElements(
+		<Route
+			path="/"
+			element={
+				<Layout>
+					<Login />
+				</Layout>
+			}
+		></Route>
+	)
+);
+
+createRoot(document.getElementById("root")!).render(
+	<RouterProvider router={router} />
+);

d9816b6fb860cb4f1e0c4eae3ffca61db7dc6b84
Arthur <155009708+ArthurGosi2024@users.noreply.github.com>
2024-06-11 13:57:29 -0300

    refactor: refactoring to nested routes

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -8,20 +8,20 @@ import {
 } from "react-router-dom";
 import Layout from "./pages/layout";
 import Login from "./pages/login/page";
+import React from "react";
+import Register from "./pages/register";
 
 const router = createBrowserRouter(
 	createRoutesFromElements(
-		<Route
-			path="/"
-			element={
-				<Layout>
-					<Login />
-				</Layout>
-			}
-		></Route>
+		<Route path="/" element={<Layout />}>
+			<Route path="/" element={<Login />} />
+			<Route path="/register" element={<Register />} />
+		</Route>
 	)
 );
 
 createRoot(document.getElementById("root")!).render(
-	<RouterProvider router={router} />
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>
 );

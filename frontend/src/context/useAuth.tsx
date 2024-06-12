0b046b7a23daad4fc932adb90aaec15547bcac1e
Arthur <155009708+ArthurGosi2024@users.noreply.github.com>
2024-06-12 13:25:48 -0300

    fix: fixing request errors because of the class validator in the backend

--- frontend/src/context/useAuth.tsx
+++ frontend/src/context/useAuth.tsx
@@ -3,6 +3,8 @@ import { IAuthContext, IUser } from "../@types/IAuth";
 import axios from "axios";
 import { IResponse } from "../@types/IResponse";
 
+import { configAxios } from "../api/config";
+
 const Auth = createContext<IAuthContext>({
 	signIn: () => {},
 	register: () => {},
@@ -16,29 +18,44 @@ interface AuthProps {
 
 export default function AuthProvider({ children }: AuthProps) {
 	const [token, setToken] = useState<string>("");
-
 	const signIn = async ({ email, password }: IUser): Promise<IResponse> => {
-		const result = await axios.get("/signIn", {
-			data: { email, password },
-		});
+		try {
+			const result = await axios.post(
+				"/signIn",
+				{ email, password },
+				{
+					...configAxios,
+				}
+			);
 
-		if (result.status == 201) {
-			setToken(result.data.token);
+			if (result.status == 201) {
+				setToken(result.data.token);
+				return {
+					status: true,
+					message: result.data.massage,
+				};
+			}
+		} catch (error: any) {
 			return {
-				status: true,
-				message: result.data.massage,
+				status: false,
+				message: error?.response?.data!.message[0],
 			};
 		}
+
 		return {
 			status: false,
-			message: result.data.massage,
+			message: "Error de requisição",
 		};
 	};
 
 	const register = async ({ email, password }: IUser): Promise<IResponse> => {
-		const result = await axios.post("/register", {
-			data: { email, password },
-		});
+		const result = await axios.post(
+			"/register",
+			{ email, password },
+			{
+				...configAxios,
+			}
+		);
 
 		if (result.status == 201) {
 			setToken(result.data.token);
@@ -55,6 +72,7 @@ export default function AuthProvider({ children }: AuthProps) {
 	const signOut = async (token: string): Promise<IResponse> => {
 		const result = await axios.post("/signOut", {
 			data: { token: token },
+			...configAxios,
 		});
 
 		if (result.status == 201) {

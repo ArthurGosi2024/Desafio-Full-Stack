05be8743f49899eedd7dd4e35c8a421252426f5c
Arthur <155009708+ArthurGosi2024@users.noreply.github.com>
2024-06-12 18:12:01 -0300

    refactor: update names

--- backend/src/modules/user/user.controller.ts
+++ backend/src/modules/user/user.controller.ts
@@ -1,19 +1,19 @@
 import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { UserService } from './user.service';
-import { SignInUser } from '../../DTOs/signIn-user';
-import { RegisterUser } from '../../DTOs/register-user';
+import { SignInUserDTO } from '../../DTOs/signIn-user-dto';
+import { RegisterUserDTO } from '../../DTOs/register-user-dto';
 
 @Controller('')
 export class UserController {
   constructor(private readonly useService: UserService) {}
 
   @Post('signIn')
-  async signIn(@Body() user: SignInUser) {
+  async signIn(@Body() user: SignInUserDTO) {
     return await this.useService.signIn(user);
   }
 
   @Post('register')
-  async register(@Body() user: RegisterUser) {
+  async register(@Body() user: RegisterUserDTO) {
     return await this.useService.register(user);
   }
 }

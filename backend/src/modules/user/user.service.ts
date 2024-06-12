e4f0b0303e4b75cfad4549dc1852582f2520b210
Arthur <155009708+ArthurGosi2024@users.noreply.github.com>
2024-06-12 18:13:06 -0300

    refactor: rearranged layer

---
+++ backend/src/modules/user/user.service.ts
@@ -0,0 +1,46 @@
+import { HttpStatus, Injectable, UnauthorizedException } from '@nestjs/common';
+import { UserRepositories } from './user.repositories';
+import { SignInUserDTO } from '../../DTOs/signIn-user-dto';
+import { JwtService } from '@nestjs/jwt';
+import { RegisterUserDTO } from '../../DTOs/register-user-dto';
+import { ErrorHttpException } from '../../exceptions/error-http';
+
+@Injectable()
+export class UserService {
+  constructor(
+    private readonly repo: UserRepositories,
+    private readonly jwt: JwtService,
+  ) {}
+
+  async signIn(user: SignInUserDTO): Promise<{ acess_token: string }> {
+    const findUser = await this.repo.find(user);
+
+    if (findUser) {
+      return {
+        acess_token: await this.jwt.signAsync(user),
+      };
+    }
+
+    throw new ErrorHttpException(
+      'Usuario não encontrado.',
+      HttpStatus.CONFLICT,
+    );
+  }
+
+  async register(user: RegisterUserDTO): Promise<{ acess_token: string }> {
+    const findUser = await this.repo.find(user);
+
+    if (findUser) {
+      throw new ErrorHttpException(
+        'Email ja existente, por favor utilize outro.',
+        HttpStatus.CONFLICT,
+      );
+    }
+
+    await this.repo.create(user);
+
+    return {
+      acess_token: await this.jwt.signAsync(user),
+    };
+  }
+}

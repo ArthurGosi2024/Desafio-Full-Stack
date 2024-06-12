975555f2f14d42eb56272c71d6b79a0d124e81de
Arthur <155009708+ArthurGosi2024@users.noreply.github.com>
2024-06-12 18:08:45 -0300

    refactor : remove console.log

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,10 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './user.module';
+import { AppModule } from './modules/user/user.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  console.log('dwdw');
-  
+
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);

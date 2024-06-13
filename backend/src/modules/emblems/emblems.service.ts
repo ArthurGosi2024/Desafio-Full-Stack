22d1148f2884d82e4d51b310eb587fee7cc06f16
Arthur <155009708+ArthurGosi2024@users.noreply.github.com>
2024-06-13 12:23:27 -0300

    add: implementing the methods

--- backend/src/modules/emblems/emblems.service.ts
+++ backend/src/modules/emblems/emblems.service.ts
@@ -1,19 +1,48 @@
-import { Injectable } from '@nestjs/common';
+import { HttpStatus, Injectable } from '@nestjs/common';
 import { EmblemsRepositories } from './emblems.repositories';
+import { ErrorHttpException } from 'src/exceptions/error-http';
+import { IEmblemsProps } from './interfaces/IEmblems';
+import { UseCaseEmblemsWithUser } from './use-cases/use-case-emblems-with-user';
+import { IPackageEmblems } from './interfaces/IPackageEmblems';
 
 @Injectable()
 export class EmblemsService {
-  constructor(private readonly emblemsRepositories: EmblemsRepositories) {}
+  constructor(
+    private readonly emblemsRepositories: EmblemsRepositories,
+    private readonly emblemsUserCase: UseCaseEmblemsWithUser,
+  ) {}
 
-  async createEmblems(): Promise<boolean> {
-    return false;
+ 
+  async getAllEmblems(): Promise<IEmblemsProps[] | []> {
+    const findAll = await this.emblemsRepositories.findAll();
+
+    if (findAll.length == 0) {
+      throw new ErrorHttpException(
+        'Não foi encontrado, nenhum emblema.',
+        HttpStatus.NOT_FOUND,
+      );
+    }
+    return findAll;
   }
-  async updateAllElements(): Promise<boolean> {
-    return false;
+
+  async getByEmblems(slug: string): Promise<IEmblemsProps | {}> {
+    const find = await this.emblemsRepositories.findByEmblems(slug);
+
+    if (!find) {
+      throw new ErrorHttpException(
+        'Não foi encontrado, nenhum emblema.',
+        HttpStatus.NOT_FOUND,
+      );
+    }
+    return find ?? false;
+  }
+
+
+  async createEmblemsUser(packageEmblems: IPackageEmblems) {
+    return await this.emblemsUserCase.addBadgeToUser(packageEmblems);
+  }
+
+  async removerEmblemsUser(packageEmblems: IPackageEmblems) {
+    return await this.emblemsUserCase.removeUserEmblem(packageEmblems);
   }
 }
-
-// criar novos emblemas
-
-// atualizar todos os emblemas
-// listar todos os emblems

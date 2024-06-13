fc7f8b9410fcd03f3d4d5dfc2a188a61b3af15a5
Arthur <155009708+ArthurGosi2024@users.noreply.github.com>
2024-06-13 12:31:15 -0300

    add: removed jwt config from user module

--- backend/src/modules/user/user.module.ts
+++ backend/src/modules/user/user.module.ts
@@ -3,16 +3,9 @@ import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { PrismaService } from '../../prisma.service';
 import { UserRepositories } from './user.repositories';
-import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [
-    JwtModule.register({
-      global: true,
-      secret: process.env.TOKEN_SECRET,
-      signOptions: { expiresIn: '24h' },
-    }),
-  ],
+  imports: [],
   controllers: [UserController],
   providers: [UserService, PrismaService, UserRepositories],
 })

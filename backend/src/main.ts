e9e45238d8d0fbdb04dac1a26266bb84a93b2363
Arthur <155009708+ArthurGosi2024@users.noreply.github.com>
2024-06-13 12:21:46 -0300

    add :added swagger configuration

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,22 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './modules/user/user.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Sistema de Login e Resgate de Emblemas')
+    .setDescription(
+      'API para gerenciar autenticação de usuários e resgate de emblemas',
+    )
+    .setVersion('1.0')
+    
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());

3da6a3e32ff8cf84e16b87ad7296f6febb8300e2
Arthur <155009708+ArthurGosi2024@users.noreply.github.com>
2024-06-13 12:23:56 -0300

    update: updating emblems modules

--- backend/src/modules/emblems/emblems.module.ts
+++ backend/src/modules/emblems/emblems.module.ts
@@ -4,10 +4,18 @@ import { EmblemsRepositories } from './emblems.repositories';
 import { PrismaService } from 'src/prisma.service';
 import { EmblemsService } from './emblems.service';
 import { UserModule } from '../user/user.module';
+import { UseCaseEmblemsWithUser } from './use-cases/use-case-emblems-with-user';
+import { UserRepositories } from '../user/user.repositories';
 
 @Module({
   imports: [UserModule],
   controllers: [EmblemsController],
-  providers: [EmblemsService, PrismaService, EmblemsRepositories],
+  providers: [
+    EmblemsService,
+    PrismaService,
+    EmblemsRepositories,
+    UseCaseEmblemsWithUser,
+    UserRepositories,
+  ],
 })
 export class EmblemsModule {}

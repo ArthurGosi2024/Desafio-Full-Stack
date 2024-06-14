5ef7b1a0158783303f301a387dfc961344f17661
Arthur <155009708+ArthurGosi2024@users.noreply.github.com>
2024-06-14 11:01:56 -0300

    refactor:  changing the react router dom provider for private route manipulations

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -3,29 +3,35 @@ import {
 	RouterProvider,
 	createRoutesFromElements,
 	Route,
+	BrowserRouter,
+	Routes,
 } from "react-router-dom";
 import Layout from "./pages/layout";
 import Login from "./pages/login/page";
 import Register from "./pages/register/page";
 import { useContextApi } from "./context/useAuth";
+import LayoutDashboard from "./pages/dashboard/layout";
+import Dashboard from "./pages/dashboard/page";
+import GetEmblems from "./pages/dashboard/getEmblems/page";
 
 export default function Router() {
 	const { token } = useContextApi();
+
 	return (
-		<RouterProvider
-			router={createBrowserRouter(
-				createRoutesFromElements(
-					<Route path="/" element={<Layout />}>
-						<Route path="/" element={<Login />} />
-						<Route path="/register" element={<Register />} />
-						{token !== "" && (
-							<>
-								<Route path="/dashboard" element={<></>} />
-							</>
-						)}
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Layout />}>
+					<Route path="/" element={<Login />} />
+					<Route path="/register" element={<Register />} />
+				</Route>
+
+				{token !== "" && (
+					<Route path="/dashboard" element={<LayoutDashboard />}>
+						<Route path="/dashboard" element={<Dashboard />} />
+						<Route path="/dashboard/getEmblems" element={<GetEmblems />} />
 					</Route>
-				)
-			)}
-		/>
+				)}
+			</Routes>
+		</BrowserRouter>
 	);
 }
